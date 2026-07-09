06922b62f2056b2c2c117f25fdbf2a0fe36e71a3
alejandrakn <alejandra.cussianovi@utec.edu.pe>
2026-07-09 12:56:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  },
+    proxy: {
+      '/api': {
+        target: 'https://cs2031-2026-1-pc2-studytrack-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
